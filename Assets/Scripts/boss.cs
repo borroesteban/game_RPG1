32905ff361f509fcbe22adbe0767269825a224f4
borroesteban <101810490+borroesteban@users.noreply.github.com>
2023-05-06 17:51:27 -0300

    FIXED bug where boss wouldn't hit

--- Assets/Scripts/boss.cs
+++ Assets/Scripts/boss.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class boss : Enemy
 {
-    private Animator bossAnim;
+    //private Animator bossAnim;
     public float[] fireballSpeed = {2.5f, -2.5f};
     public float distance = 0.25f;
     public Transform[] fireballs;
 
     private void Update()
     {   
-        bossAnim = GetComponent<Animator>();
+       // bossAnim = GetComponent<Animator>();
 
         for (int i = 0; i < fireballs.Length; i++)
         {
             fireballs[i].position = transform.position + new Vector3(-Mathf.Cos(Time.time * fireballSpeed[i]) * distance, Mathf.Sin(Time.time * fireballSpeed[i]) * distance, 0);
         }
 
-        if (transform.hasChanged)
+/*         if (transform.hasChanged)
         {
             bossAnim.SetBool("isChasing", true);
             transform.hasChanged = false;
@@ -26,7 +26,7 @@ public class boss : Enemy
         else
         {
             bossAnim.SetBool("isChasing", false);
-        }
+        } */
     }
 }
 
