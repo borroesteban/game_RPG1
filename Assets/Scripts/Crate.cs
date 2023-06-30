6cb67c338d3be1d9b980fe40ef6cbb2311a2da43
borroesteban <101810490+borroesteban@users.noreply.github.com>
2023-06-29 21:28:57 -0300

    level_animation double box animation 2
    animation added to the prefab.
    piled crate prefab changed to use 2 tiles at once instead of 2 gameobjects

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Crate : Fighter
 {
+    public List<GameObject> corpseAnimation = new List<GameObject>();
+    public Transform thisObject;
+    private Vector3 thisObjectsPosition;
     protected override void Death()
     {
+        thisObject=GetComponent<Transform>();
+        thisObjectsPosition=thisObject.position;
+        if (corpseAnimation.Count != 0)
+        {
+        Instantiate(corpseAnimation[Random.Range(0, corpseAnimation.Count)], thisObjectsPosition, Quaternion.identity);
+        }
         Destroy(gameObject);
     }
 }
